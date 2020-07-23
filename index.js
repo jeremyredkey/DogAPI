ba9623c0ac488540a6c306361457bcdd6630cf93
Jeremy Redkey <jeremy.redkey@gmail.com>
2020-07-23 02:16:53 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,50 @@
+// Create an app that lets users choose to display between 1 and 50 random dog images, then prints the results to the console.
+
+// Building on the previous app, create an app that lets users choose to display between 1 and 50 random dog images, 
+// then prints the results of the images in the console, and loads those images in the DOM.
+
+// Create an app that loads a single random image for a specific breed. 
+// This app should account for the happy case when the breed is found, as well as the unhappy case when it is not.
+
+
+'use strict';
+
+function getDogImage() {
+  let userNum = $('#userInput').val();
+
+  console.log(userNum)
+const targetUrl = `https://dog.ceo/api/breeds/image/random/${userNum}`;
+  fetch(targetUrl)
+     .then(response => response.json())
+    .then(responseJson => 
+      displayResults(responseJson))
+     .catch(error => console.log(error));
+     console.log(targetUrl)
+}
+
+function displayResults(responseJson) {
+
+let imgArr = responseJson.message;
+console.log(imgArr)
+ for (let i = 0; i < imgArr.length; i++) {
+  //replace the existing image with the new one
+   $('.results-img').append(
+   `<img src="${imgArr[i]}" class="results-img">`
+  )
+ }
+
+  //display the results section
+  $('.results').removeClass('hidden');
+}
+
+function watchForm() {
+  $('form').submit(event => {
+    event.preventDefault();
+    getDogImage();
+  });
+}
+
+$(function() {
+  console.log('App loaded! Waiting for submit!');
+  watchForm();
+});
