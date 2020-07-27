c1dcca98fe237021318e322207d37400659f36a4
Jeremy Redkey <jeremy.redkey@gmail.com>
2020-07-27 15:01:50 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,56 +1,37 @@
-// Create an app that lets users choose to display between 1 and 50 random dog images, then prints the results to the console.
-
-// Building on the previous app, create an app that lets users choose to display between 1 and 50 random dog images, 
-// then prints the results of the images in the console, and loads those images in the DOM.
-
-// Create an app that loads a single random image for a specific breed. 
-// This app should account for the happy case when the breed is found, as well as the unhappy case when it is not.
-
-
 'use strict';
 
-function getDogImage() {
-  let userNum = $('#userInput').val()
-  let dogBreed = $('#dogBreed').val().toLowerCase()
-  console.log(dogBreed)
-  console.log(userNum)
-const targetUrl = `https://dog.ceo/api/breed/${dogBreed}/images/random/${userNum}`;
-  fetch(targetUrl)
-     .then(response => response.json())
+
+function getDogNumber() {
+  let dogNumber = $('#userInput').val()
+  console.log(dogNumber)
+  fetch(`https://dog.ceo/api/breeds/image/random/${dogNumber}`)
+    .then(response => response.json())
     .then(responseJson => 
       displayResults(responseJson))
-     .catch(error => console.log(error));
+    .catch(error => alert('Error: Please enter number between 1 & 50.'));
 }
 
 function displayResults(responseJson) {
-  
-let imgArr = responseJson.message;
-console.log(imgArr)
- for (let i = 0; i < imgArr.length; i++) {
-  //replace the existing image with the new one
-   $('.results').append(
-   `<img src="${imgArr[i]}" class="results-img">`
-  )
- }
+let dogNumber = $('#userInput').val()
 
-  //display the results section
-  $('.results').removeClass('hidden');
-  $('.message').removeClass('hidden');
+for(let i = 0; i < dogNumber; i++) {
+console.log(responseJson.message[i])
+  $('.entry').append(`<img src="${responseJson.message[i]}" class="results-img-${i+1}">`);
+
+  }
+   $('.results').removeClass('hidden');
+ $('.message').removeClass('hidden');
 }
-$('#undo').click(function undoButton(){
-  $('.results-img').remove();
-
-  $('.results').addClass('hidden');
-
-})
 
 
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
-    getDogImage();
+    getDogNumber();
+    $('.entry').html('');
   });
 }
+
 
 $(function() {
   console.log('App loaded! Waiting for submit!');
